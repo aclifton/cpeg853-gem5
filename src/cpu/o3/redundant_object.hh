d43b5aad79aaf85f446add445788015c0d6e8b4d
Andrew Clifton <clifton.arc@gmail.com>
2016-11-20 16:26:55 -0500

    added redundant object

---
+++ src/cpu/o3/redundant_object.hh
@@ -0,0 +1,22 @@
+ /*
+ * Authors: Andrew Clifton
+ */
+
+#ifndef __CPU_O3_REDUNDANT_OBJECT_HH__
+#define __CPU_O3_REDUNDANT_OBJECT_HH__
+
+#include <string>
+
+class RedundantObject {
+  public:
+    void setRedundant(bool redundant){
+        this->redundant = redundant;
+    }
+    bool isRedundant() const { return this->redundant; }
+
+    std::string name() const { return this->redundant ? "redundant" : "orig"; }
+  private:
+    bool redundant = false;
+};
+
+#endif
